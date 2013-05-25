afee1af542910b477c70439d3fd2645bf69f77fc
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2013-05-25 13:30:43 +0300

    experimental feature pipe

--- src/plib.erl
+++ src/plib.erl
@@ -20,7 +20,7 @@
 -module(plib).
 
 -export([
-   node/1, cast/2, call/2, call/3, emit/2, send/2, relay/3, ack/2
+   node/1, cast/2, call/2, call/3, emit/2, send/2, relay/3, ack/2, pid/1
 ]).
 
 -type(process() :: pid() | {atom(), node()} | atom()).
@@ -55,6 +55,8 @@ node(RegName)
 %% cast asynchronous request to process
 -spec(cast/2 :: (process(), any()) -> reference()).
 
+cast({pipe, _, Pid}, Msg) ->
+   plib:cast(Pid, Msg);
 cast(Pid, Msg) ->
    Tx = erlang:make_ref(),
    try erlang:send(Pid,  {'$req', {self(), Tx}, Msg}, [noconnect]) catch _:_ -> Msg end,
@@ -64,6 +66,8 @@ cast(Pid, Msg) ->
 %% emit asynchronous request to process with pid of originator
 -spec(emit/2 :: (process(), any()) -> reference()).
 
+emit({pipe, _, Pid}, Msg) ->
+   plib:emit(Pid, Msg);
 emit(Pid, Msg) ->
    try erlang:send(Pid,  {'$req', self(), Msg}, [noconnect]) catch _:_ -> Msg end.
 
@@ -71,6 +75,8 @@ emit(Pid, Msg) ->
 %%
 -spec(send/2 :: (process(), any()) -> reference()).
 
+send({pipe, _, Pid}, Msg) ->
+   plib:send(Pid, Msg);
 send(Pid, Msg) ->
    try erlang:send(Pid,  {'$req', undefined, Msg}, [noconnect]) catch _:_ -> Msg end.
 
@@ -79,8 +85,13 @@ send(Pid, Msg) ->
 -spec(call/2 :: (process(), any()) -> any()).
 -spec(call/3 :: (process(), any(), timeout()) -> any()).
 
+call({pipe, _, Pid}, Msg) ->
+   plib:call(Pid, Msg);
 call(Pid, Msg) ->
-   call(Pid, Msg, 5000).
+   plib:call(Pid, Msg, 5000).
+
+call({pipe, _, Pid}, Msg, Timeout) ->
+   plib:call(Pid, Msg, Timeout);
 
 call(Pid, Msg, Timeout) ->
    % inspired by gen:call(...) from OTP
@@ -142,6 +153,9 @@ relay(Pid, Tx, Msg) ->
 %% acknowledge transaction
 -spec(ack/2 :: (tx(), any()) -> any()).
 
+ack({pipe, Pid, _}, Msg) ->
+   plib:ack(Pid, Msg);
+
 ack({Pid, Tx}, Msg)
  when is_pid(Pid) ->
    % backward compatible with gen_server:reply
@@ -155,6 +169,19 @@ ack(Pid, Msg)
 ack(_, _Msg) ->
    undefined.
 
+%%
+%% extract transaction pid
+-spec(pid/1 :: (tx()) -> any()).
 
+pid({Pid, _Tx})
+ when is_pid(Pid) ->
+   Pid;
+
+pid(Pid)
+ when is_pid(Pid) ->
+   Pid; 
+
+pid(_) ->
+   undefined.
 
 
