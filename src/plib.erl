d5b584dfc61905bad06531e370f5b8d210f7b578
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2013-08-04 22:37:18 +0300

    added support for custom requests (not only '')

--- src/plib.erl
+++ src/plib.erl
@@ -22,16 +22,22 @@
 -export([
    node/1, 
    cast/2, 
+   cast/3,
    emit/2, 
+   emit/3, 
    send/2, 
+   send/3,
    call/2, 
    call/3, 
+   call/4,
    relay/3, 
+   relay/4, 
    ack/2, 
    pid/1
 ]).
 
 -type(process() :: pid() | {atom(), node()} | atom()).
+-type(req()     :: atom()).
 -type(tx()      :: {pid(), reference()}).
 
 %%
@@ -59,57 +65,64 @@ node(Name)
 %%
 %% cast asynchronous request to process
 -spec(cast/2 :: (process(), any()) -> reference()).
+-spec(cast/3 :: (process(), req(), any()) -> reference()).
 
-cast({pipe, _, B}, Msg) ->
-   plib:cast(B, Msg);
 cast(Pid, Msg) ->
+   cast(Pid, '$req', Msg).
+
+cast(Pid, Req, Msg) ->
    Tx = erlang:make_ref(),
-   try erlang:send(Pid,  {'$req', {self(), Tx}, Msg}, [noconnect]) catch _:_ -> Msg end,
+   try erlang:send(Pid,  {Req, {self(), Tx}, Msg}, [noconnect]) catch _:_ -> Msg end,
    Tx.
+
 
 %%
 %% emit asynchronous request to process with pid of originator
 -spec(emit/2 :: (process(), any()) -> reference()).
+-spec(emit/3 :: (process(), req(), any()) -> reference()).
 
-emit({pipe, _, B}, Msg) ->
-   plib:emit(B, Msg);
 emit(Pid, Msg) ->
-   try erlang:send(Pid,  {'$req', self(), Msg}, [noconnect]) catch _:_ -> Msg end.
+   emit(Pid, '$req', Msg).
+
+emit(Pid, Req, Msg) ->
+   try erlang:send(Pid,  {Req, self(), Msg}, [noconnect]) catch _:_ -> Msg end.
 
 %%
 %% send asynchronous request to process 
 -spec(send/2 :: (process(), any()) -> reference()).
+-spec(send/3 :: (process(), req(), any()) -> reference()).
 
-send({pipe, _, B}, Msg) ->
-   plib:send(B, Msg);
 send(Pid, Msg) ->
-   try erlang:send(Pid,  {'$req', undefined, Msg}, [noconnect]) catch _:_ -> Msg end.
+   send(Pid, '$req', Msg).
+
+send(Pid, Req, Msg) ->
+   try erlang:send(Pid,  {Req, undefined, Msg}, [noconnect]) catch _:_ -> Msg end.
 
 %%
 %% make synchronous request to process
 -spec(call/2 :: (process(), any()) -> any()).
 -spec(call/3 :: (process(), any(), timeout()) -> any()).
+-spec(call/4 :: (process(), req(), any(), timeout()) -> any()).
 
-call({pipe, _, B}, Msg) ->
-   plib:call(B, Msg);
 call(Pid, Msg) ->
-   plib:call(Pid, Msg, 5000).
+   plib:call(Pid, '$req', Msg, 5000).
 
-call({pipe, _, B}, Msg, Timeout) ->
-   plib:call(B, Msg, Timeout);
 call(Pid, Msg, Timeout) ->
+   plib:call(Pid, '$req', Msg, Timeout).
+
+call(Pid, Req, Msg, Timeout) ->
    % inspired by gen:call(...) from OTP
    try erlang:monitor(process, Pid) of
       Tx ->
-         do_call(Tx, Pid, Msg, Timeout)
+         do_call(Tx, Pid, Req, Msg, Timeout)
    catch error:_ ->
       % unable to set monitor, fall-back to node monitor
-      fb_call(Pid, Msg, Timeout) 
+      fb_call(Pid, Req, Msg, Timeout) 
    end.   
 
-do_call(Tx, Pid, Msg, Timeout) ->
+do_call(Tx, Pid, Req, Msg, Timeout) ->
    Node = plib:node(Pid),
-   catch erlang:send(Pid, {'$req', {self(), Tx}, Msg}, [noconnect]),
+   catch erlang:send(Pid, {Req, {self(), Tx}, Msg}, [noconnect]),
    receive
       {Tx, Reply} ->
          erlang:demonitor(Tx, [flush]),
@@ -123,7 +136,7 @@ do_call(Tx, Pid, Msg, Timeout) ->
       exit(timeout)
    end.
 
-fb_call(Pid, Msg, Timeout) ->
+fb_call(Pid, Req, Msg, Timeout) ->
    Node = plib:node(Pid),
    monitor_node(Node, true),
    receive
@@ -132,7 +145,7 @@ fb_call(Pid, Msg, Timeout) ->
          exit({nodedown, Node})
    after 0 -> 
       Tx = erlang:make_ref(),
-      catch erlang:send(Pid, {'$req', {self(), Tx}, Msg}, [noconnect]),
+      catch erlang:send(Pid, {Req, {self(), Tx}, Msg}, [noconnect]),
       receive
          {Tx, Reply} ->
             monitor_node(Node, false),
@@ -149,9 +162,13 @@ fb_call(Pid, Msg, Timeout) ->
 %%
 %% relay on-going request to other process
 -spec(relay/3 :: (process(), tx(), any()) -> any()). 
+-spec(relay/4 :: (process(), req(), tx(), any()) -> any()). 
 
 relay(Pid, Tx, Msg) ->
-   try erlang:send(Pid,  {'$req', Tx, Msg}, [noconnect]) catch _:_ -> Msg end.
+   relay(Pid, '$req', Tx, Msg).
+
+relay(Pid, Req, Tx, Msg) ->   
+   try erlang:send(Pid,  {Req, Tx, Msg}, [noconnect]) catch _:_ -> Msg end.
 
 %%
 %% acknowledge transaction
